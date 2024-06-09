84e80e53c689cb973ebe9f634ae5e94843b3d0ac
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-09 01:18:20 -0700

    connect the database

--- connect.php
+++ connect.php
@@ -1,3 +1,17 @@
-<?php 
+<?php  
+$HOSTNAME='localhost';
+$USERNAME='root';
+$PASSWORD='';
+$DATABaSE='signupforms';
+
+
+$con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABaSE);
+
+if($con){
+    echo "Connection sucessful";
+}else{
+    die(mysqli_error($con));
+}
+
 
 ?>
