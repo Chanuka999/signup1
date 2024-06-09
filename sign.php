b0d6a91e0c70005ea96780cb80a17142a1fc31b5
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-09 10:02:55 -0700

    add html code

--- sign.php
+++ sign.php
@@ -4,8 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>signup page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1>Sign up</h1>
+   <div class="container">
+   <form>
+    <div class="group-1">
+    <label for="e-mail">Name</label>
+    <input type="email" id="email" placeholder="enter your user name">
+    </div>
+    <div class="group-2">
+    <label for="e-mail">Password</label>
+    <input type="password" id="password" placeholder="enter your password">
+    </div>
+     <div class="group-3">
+        <button>SUBMIT</button>
+     </div>
+   </form>
+   </div>
 </body>
 </html>
