9db08592fd6cc3565223e4e53fa90b5cae58b308
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-09 18:42:25 -0700

    connect to logout file

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php  
+  session_start();
+  if(!isset($_SESSION['username'])){
+    header('location:login.php');
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome page</title>
+    <style>
+.hello{
+    text-align: center;
+    margin-top: 25px;
+    background: var(--color-bg);
+}
+ 
+.btn{
+    background-color: blue;
+    padding: 8px;
+    margin: 10px;
+    border-radius: 10%;
+}
+    
+    </style>
+</head>
+<body>
+<h1 class="hello">Welcome
+    <?php echo $_SESSION['username'];
+    ?>
+</h1>
+
+ <div class="container">
+    <a href="logout.php"><button class="btn">Log out</button></a>
+ </div>
+</body>
+</html>
